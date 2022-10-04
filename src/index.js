b158b8e2fd7665d3812edd769d126bf593acc688
Daniel Karpierz <dankarp1999@gmail.com>
2022-10-04 12:52:25 -0400

    Finish webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import hello from './hello';
+import './styles/style.css';
+import stang from './assets/stang.jpg';
+
+const stangImg = document.getElementById('stangImg');
+stangImg.src = stang;
 
 console.log(hello());
 console.log('efakjshglkjshdglkjdhsf');
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,39 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name][contenthash].js',
+		clean: true,
+		assetModuleFilename: '[name][ext]',
+	},
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
 	},
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
+				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
 			},
 		],
 	},
